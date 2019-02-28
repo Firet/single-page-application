b55ea10585dc675510b61de490091a82d826bc27
Firet <damianfcardozo@gmail.com>
2019-02-28 12:45:36 -0300

    agrega usuarios con js

--- README.md
+++
@@ -1,1 +0,0 @@
-# Single-page-application
--- scripts/script.js
+++ scripts/script.js
@@ -1,64 +1,51 @@
 //console.log("funciona");
-//
-// var arrayDeUsuarios = [
-//     {
-//     id:1,
-//     nombre: "Ana"
-//     },
-//     {
-//     id:2,
-//     nombre: "Mauro"
-//     }
-// ];
-
-// for (var i = 0; i < arrayDeUsuarios.length; i++) {
-//     console.log("arrayDeUsuarios en el indice " + i );
-//     console.log(arrayDeUsuarios[i]);
-// }
-
-
-
-
-// function mostrarUsuarios () {
-//     console.log("mostrar usuarios");
-
-// }
-
-// function mostrarPosts() {
-//     document.getElementById("contenedor-cartas").style.display = "none";
-//     document.getElementById("post-cont").style.display = "flex";
-//     document.getElementById("texto-encabezado").innerHTML = "nombreUsuario";
-//     document.getElementById("flecha").style.display = "initial";
-// }
-// function mostrarAutores() {
-//     document.getElementById("flecha").style.display = "none";
-//     document.getElementById("contenedor-cartas").style.display = "flex";
-//     document.getElementById("post-cont").style.display = "none";
-//     document.getElementById("texto-encabezado").innerHTML = "Autores";
-// }
 
 var arrayDeUsuarios = [
     {
-        nombre: "mariano", 
-        biografia: "asd",
-        imagen: "./"
-        //despues poner mas cosas 
-    }, 
-]
+    id:1,
+    nombre: "Ana"
+    },
+    {
+    id:2,
+    nombre: "Mauro"
+    }
+];
 
-for (var i = 0; i < arrayDeUsuarios.length ; i++) {
+for (var i = 0; i < arrayDeUsuarios.length; i++) {
+    console.log("arrayDeUsuarios en el indice " + i );
+    console.log(arrayDeUsuarios[i]);
+}
 
-    console.log("funciona el for " + i);
-    crearTarjeta(arrayDeUsuarios[i].nombre, arrayDeUsuarios[i].biografia);
+
+
+
+function mostrarUsuarios () {
+    console.log("mostrar usuarios");
 
 }
 
-function crearTarjeta (nombreRecibido, biografiaRecibida) {
-    console.log("crearTarjeta");
-    var nombre = nombreRecibido;
-    var biografia = biografiaRecibida;
-
-    document.getElementById("acaVaElnombre").innerText = nombre;
-    document.getElementById("acaVaLaBiografia").innerText = biografia;
-
+function mostrarPosts() {
+    // document.getElementById("contenedor-cartas").style.display = "none";
+    // document.getElementById("post-cont").style.display = "flex";
+    // document.getElementById("texto-encabezado").innerHTML = "nombreUsuario";
+    // document.getElementById("flecha").style.display = "initial";
+    console.log("Click en el div")
 }
+function mostrarAutores() {
+    document.getElementById("flecha").style.display = "none";
+    document.getElementById("contenedor-cartas").style.display = "flex";
+    document.getElementById("post-cont").style.display = "none";
+    document.getElementById("texto-encabezado").innerHTML = "Autores";
+}
+
+
+function myFunction() {
+  var para = document.createElement("P");
+  var t = document.createTextNode("This is a paragraph.");
+  para.appendChild(t);
+  document.getElementById("myDIV").appendChild(para);
+  para.className = "algo";
+  para.onclick = function () {
+      //función
+}
+
